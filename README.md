42fbe3b5ec10ac46a5e70c236a259f7499a85ac8
Nicolae Mogoreanu <mogo@google.com>
2025-06-06 13:29:23 -0700

    Update the basic scripts / notes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # exp
+
+The repository contains experimental tools and libraries.
+
+## Environment expectations
+* Designed to be built using bazel
+* Uses the ABSL libraries
+* Optimized for use with vscode
+
+## Getting started
+
+Install bazel and git stuff.
+
+```
+git clone https://github.com/mogoreanu/exp.git
+cd exp
+bazel run :my_hello
+```
--- my_hello.cc
+++ my_hello.cc
@@ -1,25 +1,41 @@
+/*
+# Basic test
+bazel run :my_hello
+
+# Tinker with ABSL logging
+# Flags that control the logging behavior are here:
+# bazel-exp/external/abseil-cpp+/absl/log/flags.cc
+bazel run :my_hello -- --stderrthreshold=0
+
+# Specifying and working with flags
+bazel run :my_hello -- --mytest=true
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "absl/strings/str_join.h"
-#include "absl/log/log.h"
-#include "absl/log/initialize.h"
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
+#include "absl/log/flags.h"
+#include "absl/strings/str_join.h"
 
 ABSL_FLAG(bool, mytest, false, "");
 
-int main(int argc, char** argv)
-{
-    absl::ParseCommandLine(argc, argv);
-    absl::InitializeLog();
-    std::vector<std::string> v = {"foo", "bar", "baz"};
-    std::string s = absl::StrJoin(v, "-");
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  absl::InitializeLog();
+  std::vector<std::string> v = {"foo", "bar", "baz"};
+  std::string s = absl::StrJoin(v, "-");
 
-    std::cout << "Joined string: " << s << "\n";
+  std::cout << "Joined string: " << s << "\n";
+  std::cout << "`mytest` flag value: " << absl::GetFlag(FLAGS_mytest) << "\n";
 
-    LOG(INFO) << "This is a log??";
+  LOG(INFO) << "This is an INFO log.";
+  LOG(WARNING) << "This is a WARNING log.";
+  LOG(ERROR) << "This is an ERROR log!";
 
-    return 0;
+  return 0;
 }
