3479ae33d641f1f2dc5193d69422203611a1d4f4
Nicolae Mogoreanu <mogo@google.com>
2025-06-11 17:01:27 -0700

    Minor changes to burst_token_bucket

--- token_bucket/burst_token_bucket.cc
+++ token_bucket/burst_token_bucket.cc
@@ -5,13 +5,13 @@ namespace mogo {
 absl::Duration BurstTokenBucket::TryGetTokens(absl::Time now,
                                               absl::Duration tokens) {
   // If the bucket would have refilled to the max_burst_tokens - use that.
-  // absl::Duration refill_tokens = max_burst_tokens_ - cur_burst_tokens_;
-  absl::Duration delay = tb_.TryGetTokens(now - max_burst_tokens_, tokens);
+  absl::Time past_with_burst = now - max_burst_tokens_;
+  absl::Duration delay = tb_.TryGetTokens(past_with_burst, tokens);
   if (delay == absl::ZeroDuration()) {
     return absl::ZeroDuration();
   }
   if (delay <= max_burst_tokens_) {
-    delay = tb_.TryGetTokens(now - max_burst_tokens_ + delay, tokens);
+    delay = tb_.TryGetTokens(past_with_burst + delay, tokens);
     DCHECK_EQ(delay, absl::ZeroDuration());
     return absl::ZeroDuration();
   }
