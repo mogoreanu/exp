0ed29669fc71e9298a863b0323dde705d8d662ca
Nicolae Mogoreanu <mogo@google.com>
2025-06-12 14:24:25 -0700

    Minor fixes to comments

--- token_bucket/simple_token_bucket.cc
+++ token_bucket/simple_token_bucket.cc
@@ -2,16 +2,16 @@
 
 namespace mogo {
 
-  absl::Duration SimpleTokenBucket::TryGetTokens(absl::Time now, absl::Duration d) {
-    if (now >= zero_time_) {
-      // If the bucket has already returned back to zero, extract tokens and
-      // allow the request through.
-      zero_time_ = now + d;
-      return absl::ZeroDuration();
-    } else {
-      return zero_time_ - now;
-    }
+absl::Duration SimpleTokenBucket::TryGetTokens(absl::Time now,
+                                               absl::Duration d) {
+  if (now >= zero_time_) {
+    // If the bucket has already returned back to zero, extract tokens and
+    // allow the request through.
+    zero_time_ = now + d;
+    return absl::ZeroDuration();
+  } else {
+    return zero_time_ - now;
   }
-
+}
 
 }  // namespace mogo
--- token_bucket/simple_token_bucket.h
+++ token_bucket/simple_token_bucket.h
@@ -1,20 +1,20 @@
 #ifndef MOGO_EXP_TOKEN_BUCKET_SIMPLE_TOKEN_BUCKET_H
 #define MOGO_EXP_TOKEN_BUCKET_SIMPLE_TOKEN_BUCKET_H
 
-#include "absl/time/time.h"
 #include "absl/strings/str_format.h"
+#include "absl/time/time.h"
 
 namespace mogo {
 
 // Refills at one second per second.
 // Tokens extracted in units of absl::Duration.
-// Has no burst.
+// Has no burst, but the first request is going to be allowed through.
 class SimpleTokenBucket {
  public:
   explicit SimpleTokenBucket(absl::Time now) : zero_time_(now) {}
 
   // Attempts to extract the specified tokens from the token bucket.
-  // Returns absl::Zero duration if the extraction was successful.
+  // Returns absl::ZeroDuration() if the extraction was successful.
   // Returns a delay that the caller should wait for until tokens are going to
   // be available.
   absl::Duration TryGetTokens(absl::Time now, absl::Duration d);
